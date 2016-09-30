21a1db5d062ceeba2b2914fc87234a1b53e874f7
Jacky Lee <aboutjax1990@gmail.com>
2016-10-01 12:22:45 +1300

    add gulp svg

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
-'use strict';
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rsp = require('remove-svg-properties').stream;
+var svgSprite = require('gulp-svg-sprite');
 
+/**
+ * Compile styles
+ */
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
@@ -13,4 +16,38 @@ gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
-gulp.task('default', ['sass', 'sass:watch'])
+
+/**
+ * SvgSprite Config
+ */
+var svgConfig = {
+  mode: {
+    symbol: { // symbol mode to build the SVG
+      inline: true, // Prepare for inline embedding, adds display none;
+      dest: '', // destination folder
+      prefix: "mo-svg-%s",
+      sprite: 'sprite.liquid', //sprite name
+      example: false, // Build sample page
+      bust: false,
+      render: {
+        scss: false,
+      }
+    }
+  },
+  svg: {
+    xmlDeclaration: false, // strip out the XML attribute
+    doctypeDeclaration: false // don't include the !DOCTYPE declaration
+  }
+};
+
+gulp.task('svg', function() {
+  gulp.src("src/svg/*.svg" )
+  .pipe(rsp.remove({
+      properties: [rsp.PROPS_FILL]
+  }))
+  .pipe(svgSprite(svgConfig))
+  .pipe(gulp.dest("snippets/"))
+});
+
+
+gulp.task('default', ['svg', 'sass', 'sass:watch'])
